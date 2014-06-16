dc0969f4cae0d5cd3a5b97ce95e5b39372285967
Joe Reid <jreid@vnet.net>
2014-06-16 18:28:43 -0500

    finished program

--- PayrollApplication/PayrollApplication/Program.cs
+++ PayrollApplication/PayrollApplication/Program.cs
@@ -14,27 +14,34 @@ namespace PayrollApplication
             List<StaffMember> staff = new List<StaffMember>();
 
             // add volunteer
+            Console.WriteLine("adding volunteer");
             Volunteer vol = new Volunteer("VolunteerFirstName", "VolunteerLastName", "volunteer@org.TLD");
             staff.Add(vol);
             // add executive with good profits
+            Console.WriteLine("adding executive with good profits");
             Executive goodExec = new Executive("GoodExecFirstName", "GoodExecLastName", "goodexecutive@org.TLD", "111-111-1111", 10000M);
             goodExec.CalculateBonus(1000000M);
             staff.Add(goodExec);
             // add executive with ok profits
+            Console.WriteLine("adding executive with ok profits");
             Executive okExec = new Executive("OKExecFirstName", "OKExecLastName", "okexecutive@org.TLD", "555-555-5555", 10000M);
             okExec.CalculateBonus(1000M);
             staff.Add(okExec);
             // add executive with negative profits
+            Console.WriteLine("adding executive with negative profits");
             Executive zeroExec = new Executive("ZeroExecFirstName", "ZeroExecLastName", "zeroexecutive@org.TLD", "666-66-6666", 10000M);
             zeroExec.CalculateBonus(-1000M);
             staff.Add(zeroExec);
             // add hourly
+            Console.WriteLine("adding hourly employee");
             Hourly norm = new Hourly("NormFirstName", "NormLastName", "norm@org.TLD", "222-222-2222", 7.25M, 29);
             staff.Add(norm);
             // add overworked hourly
+            Console.WriteLine("adding overworked employee");
             Hourly over = new Hourly("OverFirstName", "OverLastName", "over@org.TLD", "333-333-3333", 9M, 45);
             staff.Add(over);
             // add hourly with less than 0 hours
+            Console.WriteLine("adding hourly employee with less than 0 hours");
             try
             {
                 Hourly under = new Hourly("UnderFirstName", "UnderLastName", "under@org.TLD", "444-44-4444", 7.25M, -5);
@@ -45,6 +52,7 @@ namespace PayrollApplication
                 Console.WriteLine(e.Message);
             }
             // create employee (hourly) with less than zero payrate
+            Console.WriteLine("adding hourly employee with negative payrate");
             try
             {
                 Hourly poor = new Hourly("PoorFirstName", "PoorLastName", "poor@org.TLD", "666-666-6666", -10M, 60);
@@ -56,10 +64,11 @@ namespace PayrollApplication
             }
 
             // iterate through list of staffmembers and print member and pay
+            Console.WriteLine("\n\nStaff List:");
             foreach (StaffMember item in staff)
             {
-                Console.WriteLine(item.ToString());
-                Console.WriteLine("Pay: {0:c}\n", item.Pay());
+                Console.Write(item.ToString());
+                Console.WriteLine("Pay: {0:c}", item.Pay());
             }
 
             Console.ReadLine();

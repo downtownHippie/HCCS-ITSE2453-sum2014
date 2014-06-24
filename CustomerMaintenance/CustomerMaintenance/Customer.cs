68e5e5fddb9cc5bd2f57a0236f179528128140c2
Joe Reid <jreid@vnet.net>
2014-06-24 17:54:23 -0500

    initial commit from assignment

---
+++ CustomerMaintenance/CustomerMaintenance/Customer.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace CustomerMaintenance
+{
+   
+    // (c) 2010 by Mike Murach & Associates, Inc. 
+   
+    public class Customer
+	{
+		private string firstName;
+		private string lastName;
+		private string email;
+
+		public Customer()
+		{
+		}
+
+		public Customer(string firstName, string lastName, string email)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Email = email;
+		}
+
+		public string FirstName
+		{
+			get
+			{
+				return firstName;
+			}
+			set
+			{
+				firstName = value;
+			}
+		}
+
+		public string LastName
+		{
+			get
+			{
+				return lastName;
+			}
+			set
+			{
+				lastName = value;
+			}
+		}
+
+		public string Email
+		{
+			get
+			{
+				return email;
+			}
+			set
+			{
+				email = value;
+			}
+		}
+
+		public string GetDisplayText()
+		{
+			return firstName + " " + lastName + ", " + email;
+		}
+	}
+}

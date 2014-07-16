cd0cbca5059f3e4b57ec82a7a80dc06631116d45
Joe Reid <jreid@vnet.net>
2014-07-16 00:30:41 -0500

    initial commit

---
+++ UsingExtensionMethods/UsingExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UsingExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer, a phrase, or \"Quit\" to exit:");
+            string input = Console.ReadLine();
+            while (input != "Quit")
+            {
+                try
+                {
+                    Console.WriteLine("The sum of the digits is: {0}", Convert.ToInt32(input).SumOfDigits());
+                }
+                catch (Exception)
+                {
+                    if (!String.IsNullOrWhiteSpace(input))
+                        Console.WriteLine("The PigLatin translation is:\n    {0}", input.ConvertToPigLatin());
+                }
+                Console.WriteLine("Enter an integer, a phrase, or \"Quit\" to exit:");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Press <enter> to continue");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UsingExtensionMethods/UsingExtensionMethods/UsingExtensionMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace UsingExtensionMethods
+{
+    public static class UsingExtensionMethods
+    {
+        public static int SumOfDigits(this int number)
+        {
+            int[] digits = Array.ConvertAll(number.ToString().ToCharArray(),
+                element => Int32.Parse(element.ToString()));
+            int sum = 0;
+            foreach (int i in digits)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public static string ConvertToPigLatin(this string sentence)
+        {
+            string[] words = sentence.Split();
+            StringBuilder pigLatin = new StringBuilder();
+            for (int i = 0; i < words.Length; i++)
+            {
+                StringBuilder newWord = new StringBuilder();
+                for (int j = 1; j < words[i].Length; j++)
+                {
+                    newWord.Append(words[i][j]);
+                }
+                if (Char.IsUpper(words[i][0]))
+                {
+                    newWord[0] = Char.ToUpper(newWord[0]);
+                    newWord.Append(Char.ToLower(words[i][0]));
+                }
+                else
+                    newWord.Append(words[i][0]);
+                newWord.Append("ay");
+                pigLatin.Append(newWord);
+                if (i <= words.Length - 1)
+                    pigLatin.Append(" ");
+            }
+            return pigLatin.ToString();
+        }
+    }
+}
